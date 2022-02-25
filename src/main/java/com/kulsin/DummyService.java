3dfa57c1fbeb997f9d0258715858a2bbc675d8b5
singh-kulwant <singh.kulwant@gmx.com>
2022-02-25 22:03:39 +0100

    02-25: replaced sysout with logger

--- src/main/java/com/kulsin/DummyService.java
+++ src/main/java/com/kulsin/DummyService.java
@@ -1,12 +1,16 @@
 package com.kulsin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DummyService {
 
+    public static final Logger LOG = LoggerFactory.getLogger(DummyService.class);
+
     public String printMessage(String msg) {
-        System.out.println(msg);
+        LOG.info(msg);
         return msg;
     }
 
